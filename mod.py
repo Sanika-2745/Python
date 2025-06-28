362faa3081e7b6443a1bc3335c18d5aa230e1ae0
Sanika-2745 <schougale003@gmail.com>
2025-06-28 11:59:04 -0700

    Add files via upload

---
+++ mod.py
@@ -0,0 +1,7 @@
+
+def msg():
+    print("welcome to kolhapur")
+
+def info(name,age,address,profile):
+    print(name,age,address,profile)  
+    
---
+++ module.py
@@ -0,0 +1,10 @@
+
+def msg():
+    print("welcome to kolhapur")
+
+def info(name,age,address,profile):
+    print(name,age,address,profile)  
+
+def data(mob,roll_no,address):
+    print(mob,roll_no,address)  
+    
