78e5c2f68b1ffa1cf68f57258937855cce1dbabf
Sanika-2745 <schougale003@gmail.com>
2025-06-28 11:51:51 -0700

    Add files via upload

---
+++ arithmatic_operator.py
@@ -0,0 +1,20 @@
+num1=int(input("enter the num1:"))
+num2=int(input("enter the num2:"))
+
+#adition of two numbers
+add=num1+num2
+print("addition of two number:",add)
+
+#subtraction of two numbers
+sub=num1-num2
+print("substraction of numbers",sub)
+
+#multiplication of two numbers
+mul=num1*num2
+print("multiplication of to numbers",mul)
+
+#division of two numbers
+div=num1/num2
+print("divison of two numbers",div)
+
+ 
