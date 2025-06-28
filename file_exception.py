cd3624abefbb29ee2548c0827a5dc18b992c114b
Sanika-2745 <schougale003@gmail.com>
2025-06-28 11:53:51 -0700

    Add files via upload

---
+++ file_exception.py
@@ -0,0 +1,14 @@
+
+no1 = int(input("enter no1:"))
+no2 = int(input("enter no2:"))
+
+
+try:
+    res = no1 / no2
+    print(res)
+except:
+    print("cannot divide by zero")
+else:
+    print("all is well")
+        
+        
