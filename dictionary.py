09b8e921c20761c1d2c9c8cc637e6d7e537b7ea2
Sanika-2745 <schougale003@gmail.com>
2025-06-28 11:53:07 -0700

    Add files via upload

---
+++ dictionary.py
@@ -0,0 +1,45 @@
+#dictionary example:
+student={
+    "fname":"sanika",
+    "lname":"chougale",
+    "age":20,
+    "hobby":"swimming",
+    "address":"radhanagari",
+    "grade":"A"
+}
+print(student)
+# #access item :
+# print(student["hobby"])
+# print(student["fname"])
+# #changing item:
+# student["age"]=21
+# print(student)
+# #update:
+# student.update({"grade":"B"})
+# print(student)     
+
+#remove item:
+# student.pop("grade")
+# print(student) 
+# student.popitem()
+# print(student)
+# student.clear()
+# print(student)
+# del student
+# print(student)
+
+# #looping:
+# for i in student:
+#     print(str(i)+" "+str(student[i])) 
+
+# for i in student.keys():
+#     print(i)    
+
+# for i in student.values():
+#     print(i)
+
+# for i in student.items():
+#     print(i)
+
+
+    
