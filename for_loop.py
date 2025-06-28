a5926d64a0e54a0bee65278501d584fa01765fb8
Sanika-2745 <schougale003@gmail.com>
2025-06-28 11:54:48 -0700

    Add files via upload

---
+++ for_loop.py
@@ -0,0 +1,45 @@
+# collage="KPIT"
+# print(collage)
+# #case1:
+
+# for i in collage:
+#     print(i)
+
+# # ##case2:
+# for i in collage:
+#     print(i,end=" ")
+
+#find the length:
+# for i in range(len(collage)):
+#     print(i,collage[i])
+
+
+# fruits=["mango","apple","graps","banana"]
+# print(fruits)
+# for i in range (len(fruits)):
+#     print(i,fruits[i])
+
+# for i in range(1,101,1):
+#     print(i,end=" ")
+
+
+# for i in range(100,0,-1):
+#     print(i,end=" ")
+
+
+# actors=["aditya","sushant","akshay","tiger","Anil"]
+# print(actors)
+# for i in actors:
+#     print(i)
+#     if i=="sushant":
+#         break
+
+# for i in range (1,5,1):
+#     if i==3:
+#         continue
+#     print(i)
+    
+
+
+
+
