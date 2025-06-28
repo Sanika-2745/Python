bd6f065aa619f96488aff9b9d638cfe879cfc96d
Sanika-2745 <schougale003@gmail.com>
2025-06-28 11:51:35 -0700

    Add files via upload

---
+++ area_of_all_shape.py
@@ -0,0 +1,19 @@
+# # #area of rectangle
+
+# length=int(input("enter length:"))
+# width=int(input("enter width:"))
+
+# rec_area=length*width
+# print("area of rectangle:",rec_area)
+
+# # #Area of square
+
+# side=int(input("enter side:"))
+# squ_area=side*side
+# print("area of square:",squ_area)
+
+# #area of circle
+pi=3.14
+radius=int(input("enter the radius:"))
+cir_area=pi*radius*radius
+print("area of circle:",cir_area)
