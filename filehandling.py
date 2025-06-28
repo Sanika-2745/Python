35a6b843e0aa882ad7477e9c24e7fab5763ac29c
Sanika-2745 <schougale003@gmail.com>
2025-06-28 11:54:13 -0700

    Add files via upload

---
+++ filehandling.py
@@ -0,0 +1,13 @@
+
+f = open("info.txt","w")
+print(f)
+print("file name:",f.name)
+print("file closed:",f.closed)
+print("file mode:",f.mode)
+# f.close()
+# print(f.closed)
+# print(f.write("welcome to igap technology"))
+# print(f.read())
+
+# import os
+# os.remove("info.txt")
