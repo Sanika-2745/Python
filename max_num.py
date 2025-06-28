acfc982585d5e88cb29958d16b5f041aead25167
Sanika-2745 <schougale003@gmail.com>
2025-06-28 11:56:55 -0700

    Add files via upload

---
+++ max_num.py
@@ -0,0 +1,11 @@
+#write a pogram find maximum numbers
+num1=int(input("enter the num1:"))
+num2=int(input("enter the num2:"))
+
+if num1>num2:
+    print("num1 is maximum")
+elif num2>num1:
+    print("num2 is maximum")
+else:
+    print("all num is same")
+    
