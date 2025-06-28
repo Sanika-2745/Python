eda0df2d9a7a43f6032ae5750708193a4853fbf1
Sanika-2745 <schougale003@gmail.com>
2025-06-28 11:51:04 -0700

    Add files via upload

---
+++ all_star_pattern.py
@@ -0,0 +1,62 @@
+#quation1:
+
+# num=int(input("enter the num:"))
+# for i in range (1,num+1,1):
+#     for j in range(1,i+1,1):
+#         print("*",end="")
+#     print()    
+    
+#Quation 2:
+# num=int(input("enter the num:"))
+# for i in range (num,0,-1):
+#     for j in range(1,i+1,1):
+#         print("*",end="")
+#     print()  
+
+
+# #Q3:
+# num=int (input("enter num:"))
+# for i in range (1,num+1,1):
+#     for j in range(num+1,i,-1):
+#         print(" ",end=" ")
+#     for k in range(1,i+1,1):
+#         print("* ",end="   ")
+#     print()
+
+
+#Q3:
+# num=int(input("enter num:"))
+# for i in range (num,0,-1):
+#     for j in range(num+1,i,-1):
+#         print(" ",end=" ")
+#     for k in range(1,i+1,1):
+#         print("* ",end="  ")
+#     print()
+
+#print daimond star pattern:
+# num=int(input("enter num:"))
+# for i in range (1,num+1,1):
+#     for j in range(num+1,i,-1):
+#         print(" ",end=" ")
+#     for k in range(1,i+1,1):
+#         print("* ",end="  ")
+#     print()
+
+# for i in range (num-1,0,-1):
+#     for j in range(num+1,i,-1):
+#         print(" ",end=" ")
+#     for k in range(1,i+1,1):
+#         print("* ",end="  ")
+#     print()  
+
+#num=int(input("enter num:"))
+# for i in range (1,num+1,1):
+#     for j in range(num+1,i,-1):
+#         print(" ",end=" ")
+#     for k in range(1,i+1,1):
+#         print("* ",end="  ")
+#     print()
+
+for i in range (1,num,1):
+    for j in range(num+1,i,-1):
+        print(" ",end=" ")
